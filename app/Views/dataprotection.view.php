588dfc996f6822bfb4e2550f8cd79b3b665b861d
ismaelmurad <i.murad@galawork.de>
2024-05-15 22:11:46 +0200

    small changes

--- app/Views/dataprotection.view.php
+++ app/Views/dataprotection.view.php
@@ -27,8 +27,8 @@ Controller::partial(
             <p><strong>2. Erhebung und Speicherung personenbezogener Daten sowie Art und Zweck von
                     deren Verwendung</strong></p>
             <p>a) Beim Besuch der Website</p>
-            <p>Beim Aufrufen unseren Websites feature-voting.ismael-murad.com werden durch den auf Ihrem Endgerät zum
-                Einsatz kommenden Browser automatisch Informationen an den Server unserer Website
+            <p>Beim Aufrufen meinen Websites feature-voting.ismael-murad.com werden durch den auf Ihrem Endgerät zum
+                Einsatz kommenden Browser automatisch Informationen an den Server meiner Website
                 gesendet. Diese Informationen werden temporär in einem sog. Logfile
                 gespeichert.<br/>
                 Folgende Informationen werden dabei ohne Ihr Zutun erfasst und bis zur
@@ -42,40 +42,40 @@ Controller::partial(
                     Ihres Access-Providers.
                 </li>
             </ul>
-            <p>Die genannten Daten werden durch uns zu folgenden Zwecken verarbeitet:</p>
+            <p>Die genannten Daten werden durch mich zu folgenden Zwecken verarbeitet:</p>
             <p>Gewährleistung eines reibungslosen Verbindungsaufbaus der Website<br/>
-                Gewährleistung einer komfortablen Nutzung unserer Website<br/>
+                Gewährleistung einer komfortablen Nutzung meiner Website<br/>
                 Auswertung der Systemsicherheit und -stabilität sowie<br/>
                 zu weiteren administrativen Zwecken.</p>
             <p>Die Rechtsgrundlage für die Datenverarbeitung ist Art. 6 Abs. 1 S. 1 lit. f DSGVO.
-                Unser berechtigtes Interesse folgt aus oben aufgelisteten Zwecken zur Datenerhebung.
-                In keinem Fall verwenden wir die erhobenen Daten zu dem Zweck, Rückschlüsse auf Ihre
-                Person zu ziehen. Darüber hinaus setzen wir beim Besuch unserer Website Cookies
+                mein berechtigtes Interesse folgt aus oben aufgelisteten Zwecken zur Datenerhebung.
+                In keinem Fall verwende ich die erhobenen Daten zu dem Zweck, Rückschlüsse auf Ihre
+                Person zu ziehen. Darüber hinaus setze ich beim Besuch meiner Website Cookies
                 sowie Analysedienste ein. Nähere Erläuterungen dazu erhalten Sie unter den Ziff. 4
                 und 5 dieser Datenschutzerklärung.</p>
-            <p>b) Bei Nutzung unseres Kontaktformulars</p>
-            <p>Bei Fragen jeglicher Art bieten wir Ihnen die Möglichkeit, mit uns über ein auf der
+            <p>b) Bei Nutzung meines Kontaktformulars</p>
+            <p>Bei Fragen jeglicher Art biete ich Ihnen die Möglichkeit, mit mir über ein auf der
                 Website bereitgestelltes Formular Kontakt aufzunehmen. Dabei ist die Angabe einer
-                gültigen E-Mail-Adresse erforderlich, damit wir wissen, von wem die Anfrage stammt
+                gültigen E-Mail-Adresse erforderlich, damit ich weiß, von wem die Anfrage stammt
                 und um diese beantworten zu können. Weitere Angaben können freiwillig getätigt
-                werden. Die Datenverarbeitung zum Zwecke der Kontaktaufnahme mit uns erfolgt nach
+                werden. Die Datenverarbeitung zum Zwecke der Kontaktaufnahme mit mir erfolgt nach
                 Art. 6 Abs. 1 S. 1 lit. a DSGVO auf Grundlage Ihrer freiwillig erteilten
-                Einwilligung. Die für die Benutzung des Kontaktformulars von uns erhobenen
+                Einwilligung. Die für die Benutzung des Kontaktformulars von mir erhobenen
                 personenbezogenen Daten werden nach Erledigung der von Ihnen gestellten Anfrage
                 automatisch gelöscht.</p>
             <p>c) Bei Eröffnung eines Kundenkontos und zur Vertragsabwicklung</p>
-            <p>Gemäß Art. 6 Abs. 1 lit. b DSGVO erheben und verarbeiten wir personenbezogene Daten
-                ferner, wenn Sie uns diese von sich aus mitteilen, um einen Vertrag durchzuführen
-                oder ein Kundenkonto auf unserer Website einzurichten. Die erhobenen Daten ergeben
-                sich aus den Eingabeformularen, die wir bei Bestellvorgängen oder bei der Anlegung
-                eines Kundekontos für Sie bereithalten. Selbstverständlich können Sie bei uns auch
-                als Gast bestellen und müssen kein Kundenkonto einrichten, aber auch dann speichern
-                wir die für die Vertragsabwicklung notwendigen Daten. Sie können Ihr Kundenkonto
+            <p>Gemäß Art. 6 Abs. 1 lit. b DSGVO erheben und verarbeite ich personenbezogene Daten
+                ferner, wenn Sie mir diese von sich aus mitteilen, um einen Vertrag durchzuführen
+                oder ein Kundenkonto auf meiner Website einzurichten. Die erhobenen Daten ergeben
+                sich aus den Eingabeformularen, die ich bei Bestellvorgängen oder bei der Anlegung
+                eines Kundekontos für Sie bereithalte. Selbstverständlich können Sie bei mir auch
+                als Gast bestellen und müssen kein Kundenkonto einrichten, aber auch dann speichere
+                ich die für die Vertragsabwicklung notwendigen Daten. Sie können Ihr Kundenkonto
                 jederzeit löschen lassen. Nach Löschung Ihres Kundenkontos werden die Daten unter
                 Berücksichtigung der steuer- und handelsrechtlichen Aufbewahrungsfristen weiter
                 gespeichert und nach deren Ablauf gelöscht, es sei denn es liegt weiter ein
                 berechtigtes Interesse an der Speicherung der Daten vor. Zur Abwicklung eines
-                Vertrages mit uns speichern und verwenden wir ebenfalls die von Ihnen mitgeteilten
+                Vertrages mit mir speichern und verwende ich ebenfalls die von Ihnen mitgeteilten
                 Daten. Nach vollständiger Abwicklung des Vertrages und unter Berücksichtigung der
                 vorgenannten handels- und steuerrechtlichen Aufbewahrungsfristen werden Ihre Daten
                 gelöscht, es sei denn es liegt weiter ein berechtigtes Interesse zur Speicherung
@@ -83,7 +83,7 @@ Controller::partial(
             <p><strong>3. Weitergabe von Daten</strong></p>
             <p>Eine Übermittlung Ihrer persönlichen Daten an Dritte zu anderen als den im Folgenden
                 aufgeführten Zwecken findet nicht statt.</p>
-            <p>Wir geben Ihre persönlichen Daten nur an Dritte weiter, wenn:</p>
+            <p>Ich gib Ihre persönlichen Daten nur an Dritte weiter, wenn:</p>
             <p>Sie Ihre nach Art. 6 Abs. 1 S. 1 lit. a DSGVO ausdrückliche Einwilligung dazu erteilt
                 haben, die Weitergabe nach Art. 6 Abs. 1 S. 1 lit. f DSGVO zur Geltendmachung,
                 Ausübung oder Verteidigung von Rechtsansprüchen erforderlich ist und kein Grund zur
@@ -93,69 +93,69 @@ Controller::partial(
                 gesetzlich zulässig und nach Art. 6 Abs. 1 S. 1 lit. b DSGVO für die Abwicklung von
                 Vertragsverhältnissen mit Ihnen erforderlich ist.</p>
             <p><strong>4. Cookies</strong></p>
-            <p>Wir setzen auf unserer Seite Cookies ein. Hierbei handelt es sich um kleine Dateien,
+            <p>ich setze auf meiner Seite Cookies ein. Hierbei handelt es sich um kleine Dateien,
                 die Ihr Browser automatisch erstellt und die auf Ihrem Endgerät keinen Schaden
                 anrichten. Die Cookies enthalten keine Viren, Trojaner oder sonstige Schadsoftware.
                 In dem Cookie werden Informationen abgelegt, die sich jeweils im Zusammenhang mit
-                dem spezifisch eingesetzten Endgerät ergeben. Dies bedeutet jedoch nicht, dass wir
-                dadurch unmittelbar Kenntnis von Ihrer Identität erhalten. Der Einsatz von Cookies
-                dient einerseits dazu, die Nutzung unseres Angebots für Sie angenehmer zu gestalten.
-                So setzen wir sogenannte Session-Cookies ein, um zu erkennen, dass Sie einzelne
-                Seiten unserer Website bereits besucht haben. Diese werden nach Verlassen unserer
-                Seite automatisch gelöscht. Darüber hinaus setzen wir ebenfalls zur Optimierung der
+                dem spezifisch eingesetzten Endgerät ergeben. Dies bedeutet jedoch nicht, dass Ich
+                dadurch unmittelbar Kenntnis von Ihrer Identität erhalte. Der Einsatz von Cookies
+                dient einerseits dazu, die Nutzung meines Angebots für Sie angenehmer zu gestalten.
+                So setze ich sogenannte Session-Cookies ein, um zu erkennen, dass Sie einzelne
+                Seiten meiner Website bereits besucht haben. Diese werden nach Verlassen meiner
+                Seite automatisch gelöscht. Darüber hinaus setze ich ebenfalls zur Optimierung der
                 Benutzerfreundlichkeit temporäre Cookies ein, die für einen bestimmten festgelegten
-                Zeitraum auf Ihrem Endgerät gespeichert werden. Besuchen Sie unsere Seite erneut, um
-                unsere Dienste in Anspruch zu nehmen, wird automatisch erkannt, dass Sie bereits bei
-                uns waren und welche Eingaben und Einstellungen sie getätigt haben, um diese nicht
-                noch einmal eingeben zu müssen. Zum anderen setzten wir Cookies ein, um die Nutzung
-                unserer Website statistisch zu erfassen und zum Zwecke der Optimierung unseres
-                Angebotes für Sie auszuwerten (siehe Ziff. 5). Diese Cookies ermöglichen es uns, bei
-                einem erneuten Besuch unserer Seite automatisch zu erkennen, dass Sie bereits bei
-                uns waren. Diese Cookies werden nach einer jeweils definierten Zeit automatisch
+                Zeitraum auf Ihrem Endgerät gespeichert werden. Besuchen Sie meiner Seite erneut, um
+                meiner Dienste in Anspruch zu nehmen, wird automatisch erkannt, dass Sie bereits bei
+                mir waren und welche Eingaben und Einstellungen sie getätigt haben, um diese nicht
+                noch einmal eingeben zu müssen. Zum anderen setzte ich Cookies ein, um die Nutzung
+                meiner Website statistisch zu erfassen und zum Zwecke der Optimierung meines
+                Angebotes für Sie auszuwerten (siehe Ziff. 5). Diese Cookies ermöglichen es mir, bei
+                einem erneuten Besuch meiner Seite automatisch zu erkennen, dass Sie bereits bei
+                mir waren. Diese Cookies werden nach einer jeweils definierten Zeit automatisch
                 gelöscht. Die durch Cookies verarbeiteten Daten sind für die genannten Zwecke zur
-                Wahrung unserer berechtigten Interessen sowie der Dritter nach Art. 6 Abs. 1 S. 1
+                Wahrung meiner berechtigten Interessen sowie der Dritte nach Art. 6 Abs. 1 S. 1
                 lit. f DSGVO erforderlich. Die meisten Browser akzeptieren Cookies automatisch. Sie
                 können Ihren Browser jedoch so konfigurieren, dass keine Cookies auf Ihrem Computer
                 gespeichert werden oder stets ein Hinweis erscheint, bevor ein neuer Cookie angelegt
                 wird. Die vollständige Deaktivierung von Cookies kann jedoch dazu führen, dass Sie
-                nicht alle Funktionen unserer Website nutzen können.</p>
+                nicht alle Funktionen meiner Website nutzen können.</p>
             <p><strong>5. Dauer der Speicherung der personenbezogenen Daten</strong></p>
             <p>Ihre personenbezogenen Daten werden entsprechend den jeweiligen gesetzlichen, z.B.
-                steuerrechtlichen oder handelsrechtlichen Aufbewahrungsfristen bei uns gespeichert.
+                steuerrechtlichen oder handelsrechtlichen Aufbewahrungsfristen bei mir gespeichert.
                 Nach Ablauf dieser gesetzlichen Fristen werden die Daten gelöscht, es sei denn diese
                 sind weiter für die Vertragserfüllung oder Vertragsanbahnung erforderlich oder es
                 besteht ein berechtigtes Interesse an der Weiterspeicherung.</p>
             <p><strong>6. Betroffenenrechte</strong></p>
             <p>Sie haben das Recht:</p>
-            <p>gemäß Art. 15 DSGVO Auskunft über Ihre von uns verarbeiteten personenbezogenen Daten
+            <p>gemäß Art. 15 DSGVO Auskunft über Ihre von mir verarbeiteten personenbezogenen Daten
                 zu verlangen. Insbesondere können Sie Auskunft über die Verarbeitungszwecke, die
                 Kategorie der personenbezogenen Daten, die Kategorien von Empfängern, gegenüber
                 denen Ihre Daten offengelegt wurden oder werden, die geplante Speicherdauer, das
                 Bestehen eines Rechts auf Berichtigung, Löschung, Einschränkung der Verarbeitung
                 oder Widerspruch, das Bestehen eines Beschwerderechts, die Herkunft ihrer Daten,
-                sofern diese nicht bei uns erhoben wurden, sowie über das Bestehen einer
+                sofern diese nicht bei mir erhoben wurden, sowie über das Bestehen einer
                 automatisierten Entscheidungsfindung einschließlich Profiling und ggf.
                 aussagekräftigen Informationen zu deren Einzelheiten verlangen gemäß Art. 16 DSGVO
-                unverzüglich die Berichtigung unrichtiger oder Vervollständigung Ihrer bei uns
+                unverzüglich die Berichtigung unrichtiger oder Vervollständigung Ihrer bei mir
                 gespeicherten personenbezogenen Daten zu verlangen gemäß Art. 17 DSGVO die Löschung
-                Ihrer bei uns gespeicherten personenbezogenen Daten zu verlangen, soweit nicht die
+                Ihrer bei mir gespeicherten personenbezogenen Daten zu verlangen, soweit nicht die
                 Verarbeitung zur Ausübung des Rechts auf freie Meinungsäußerung und Information, zur
                 Erfüllung einer rechtlichen Verpflichtung, aus Gründen des öffentlichen Interesses
                 oder zur Geltendmachung, Ausübung oder Verteidigung von Rechtsansprüchen
                 erforderlich ist gemäß Art. 18 DSGVO die Einschränkung der Verarbeitung Ihrer
                 personenbezogenen Daten zu verlangen, soweit die Richtigkeit der Daten von Ihnen
                 bestritten wird, die Verarbeitung unrechtmäßig ist, Sie aber deren Löschung ablehnen
-                und wir die Daten nicht mehr benötigen, Sie jedoch diese zur Geltendmachung,
+                und ich die Daten nicht mehr benötigen, Sie jedoch diese zur Geltendmachung,
                 Ausübung oder Verteidigung von Rechtsansprüchen benötigen oder Sie gemäß Art. 21
                 DSGVO Widerspruch gegen die Verarbeitung eingelegt haben gemäß Art. 20 DSGVO Ihre
-                personenbezogenen Daten, die Sie uns bereitgestellt haben, in einem strukturierten,
-                gängigen und maschinenlesebaren Format zu erhalten oder die Übermittlung an einen
+                personenbezogenen Daten, die Sie mir bereitgestellt haben, in einem strukturierten,
+                gängigen und maschinenlesbaren Format zu erhalten oder die Übermittlung an einen
                 anderen Verantwortlichen zu verlangen gemäß Art. 7 Abs. 3 DSGVO Ihre einmal erteilte
-                Einwilligung jederzeit gegenüber uns zu widerrufen. Dies hat zur Folge, dass wir die
+                Einwilligung jederzeit gegenüber mir zu widerrufen. Dies hat zur Folge, dass ich die
                 Datenverarbeitung, die auf dieser Einwilligung beruhte, für die Zukunft nicht mehr
-                fortführen dürfen und gemäß Art. 77 DSGVO sich bei einer Aufsichtsbehörde zu
+                fortführen darf und gemäß Art. 77 DSGVO sich bei einer Aufsichtsbehörde zu
                 beschweren. In der Regel können Sie sich hierfür an die Aufsichtsbehörde Ihres
-                üblichen Aufenthaltsortes oder Arbeitsplatzes oder unseres Unternehmenssitzes
+                üblichen Aufenthaltsortes oder Arbeitsplatzes oder meines Sitzes
                 wenden. Sie finden eine Liste der Datenschutzbeauftragten und deren Kontaktdaten
                 unter <a
                         href="https://www.bfdi.bund.de/DE/Infothek/Anschriften_Links/anschriften_links-node.html"
@@ -168,25 +168,25 @@ Controller::partial(
                 einzulegen, soweit dafür Gründe vorliegen, die sich aus Ihrer besonderen Situation
                 ergeben oder sich der Widerspruch gegen Direktwerbung richtet. Im letzteren Fall
                 haben Sie ein generelles Widerspruchsrecht, das ohne Angabe einer besonderen
-                Situation von uns umgesetzt wird. Möchten Sie von Ihrem Widerrufs- oder
+                Situation von mir umgesetzt wird. Möchten Sie von Ihrem Widerrufs- oder
                 Widerspruchsrecht Gebrauch machen, genügt eine E-Mail an job@ismael-murad.com</p>
             <p><strong>8. Datensicherheit</strong></p>
-            <p>Wir verwenden innerhalb des Website-Besuchs das verbreitete SSL-Verfahren (Secure
+            <p>Ich verwende innerhalb des Website-Besuchs das verbreitete SSL-Verfahren (Secure
                 Socket Layer) in Verbindung mit der jeweils höchsten Verschlüsselungsstufe, die von
                 Ihrem Browser unterstützt wird. In der Regel handelt es sich dabei um eine 256 Bit
                 Verschlüsselung. Falls Ihr Browser keine 256-Bit Verschlüsselung unterstützt,
-                greifen wir stattdessen auf 128-Bit v3 Technologie zurück. Ob eine einzelne Seite
-                unseres Internetauftrittes verschlüsselt übertragen wird, erkennen Sie an der
+                greife ich stattdessen auf 128-Bit v3 Technologie zurück. Ob eine einzelne Seite
+                meines Internetauftrittes verschlüsselt übertragen wird, erkennen Sie an der
                 geschlossenen Darstellung des Schüssel- beziehungsweise Schloss-Symbols in der
-                unteren Statusleiste Ihres Browsers. Wir bedienen uns im Übrigen geeigneter
+                unteren Statusleiste Ihres Browsers. Ich bediene mich im Übrigen geeigneter
                 technischer und organisatorischer Sicherheitsmaßnahmen, um Ihre Daten gegen
                 zufällige oder vorsätzliche Manipulationen, teilweisen oder vollständigen Verlust,
-                Zerstörung oder gegen den unbefugten Zugriff Dritter zu schützen. Unsere
+                Zerstörung oder gegen den unbefugten Zugriff Dritter zu schützen. meine
                 Sicherheitsmaß- nahmen werden entsprechend der technologischen Entwicklung
                 fortlaufend verbessert.</p>
             <p><strong>9. Aktualität und Änderung dieser Datenschutzerklärung</strong></p>
             <p>Diese Datenschutzerklärung ist aktuell gültig und hat den Stand Mai 2020. Durch die
-                Weiterentwicklung unserer Website und Angebote darüber oder aufgrund geänderter
+                Weiterentwicklung meiner Website und Angebote darüber oder aufgrund geänderter
                 gesetzlicher beziehungsweise behördlicher Vorgaben kann es notwendig werden, diese
                 Datenschutzerklärung zu ändern. Die jeweils aktuelle Datenschutzerklärung kann
                 jederzeit auf der Website unter https://feature-voting.ismael-murad.com/datenschutzerklaerung/ von
